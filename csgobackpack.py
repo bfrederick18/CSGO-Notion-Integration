4b82b53865d4704a53488f3194838239c3af2464
bfrederick18 <hitek1218@outlook.com>
2023-07-01 23:00:28 +0000

    Rename get_item_price to get_item_price_json; Work on get_item_list

--- csgobackpack.py
+++ csgobackpack.py
@@ -1,35 +1,23 @@
 import json
 import requests
-from pin import new_pin, pin_print
 
 headers = {'Content-Type': 'application/json'}
 
 
 def get_item_list():
-    pin = new_pin()
-    url = 'http://csgobackpack.net/api/GetItemsList/v2/'
-    payload = {
-        'no_prices': 'yes',
-        'no_details': 'yes',
-        'details': 'no',
-        'prettyprint': 'yes'
-    }
-    pin_print('Getting data...', pin)
-    response = requests.get(url, json=payload)
+    # url = 'http://csgobackpack.net/api/GetItemsList/v2/?no_prices=yes&no_details=yes&details=no&prettyprint=yes'
+    url = 'http://csgobackpack.net/api/GetItemsList/v2/?no_details=yes&details=no&prettyprint=yes'
+    response = requests.get(url)
     response_json = response.json()
 
     with open('csgobackpack.json', 'w') as file:
         json.dump(response_json, file, indent=4, sort_keys=True)
+        
 
-    # print(response_json)
-
-
-def get_item_price(id):
-    pin = new_pin()
+def get_item_price_json(id):
     url = f'http://csgobackpack.net/api/GetItemPrice/?id={id}'
-
-    pin_print('Getting data...', pin)
     response = requests.get(url)
     response_json = response.json()
 
-    print(response_json)
+    # print(response_json)
+    return response_json
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from notion import get_pages, print_pages
-from csgobackpack import get_item_list, get_item_price
+from csgobackpack import get_item_list, get_item_price_json
 
 ID_CURRENT_INV = os.environ['ID_CURRENT_INV']
 ID_WISHLIST = os.environ['ID_WISHLIST']
@@ -9,5 +9,5 @@ ID_WISHLIST = os.environ['ID_WISHLIST']
 # get_pages(ID_WISHLIST)
 print_pages(ID_WISHLIST)
 
-get_item_price('XM1014 | Watchdog (Factory New)')
-# get_item_list()
+get_item_price_json('XM1014 | Watchdog (Factory New)')
+get_item_list()
