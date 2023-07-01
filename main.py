79566da7389023b56a47a6dd63cded991b054871
bfrederick18 <hitek1218@outlook.com>
2023-07-01 22:20:16 +0000

    Add and integrate next_pin, new_pin, and pin_print into get_pages, read_dbs, and print_pages

--- main.py
+++ main.py
@@ -11,30 +11,53 @@ headers = {
     'Content-Type': 'application/json',
     'Notion-Version': '2022-06-28'
 }
+next_pin = 0
 
 
-def get_pages(DB_ID):
-    url = f'https://api.notion.com/v1/databases/{DB_ID}/query'
+def new_pin():
+    global next_pin
+    next_pin += 1
+    return next_pin
+
+
+def pin_print(s, a_pin):
+    print(f'{a_pin}: {s}')
+
+
+def get_pages(db_id):
+    pin = new_pin()
+    url = f'https://api.notion.com/v1/databases/{db_id}/query'
     payload = {'page_size': 100}
 
+    pin_print('Posting payload...', pin)
     response = requests.post(url, json=payload, headers=headers)
     response_json = response.json()
-    data = {DB_ID: response_json}
+    data = {db_id: response_json}
 
-    print(json.dumps(response_json, indent=4))
     with open('dbs.json', 'w') as file:
         json.dump(data, file, indent=4, sort_keys=True)
 
-    response_results = response_json['results']
-    # print(len(response_results), response_results)
-
-    # for result in response_results:
-    #     print(json.dumps(result, indent=4, sort_keys=True), end='\n\n')
-
-    return response_results
+    pin_print('Done.', pin)
 
 
-def print_pages(pages):
+def read_dbs(db_id):
+    pin = new_pin()
+    pin_print('Reading db...', pin)
+
+    pages = {}
+    with open('dbs.json', 'r') as file:
+        data = json.load(file)
+        pages = data[db_id]['results']
+
+    pin_print('Done.', pin)
+    return pages
+
+
+def print_pages(db_id):
+    pin = new_pin()
+    pin_print('Printing pages...', pin)
+
+    pages = read_dbs(db_id)
     for page in pages:
         props = page['properties']
         item_title = props['Item Name']['title']
@@ -44,9 +67,11 @@ def print_pages(pages):
             item_name = item_title[0]['plain_text']
 
         id_name = props['ID Name']['formula']['string']
-
         print(id_name)
+
+    pin_print('Done.', pin)
 
 
 # print_pages(get_pages(ID_CURRENT_INV))
-print_pages(get_pages(ID_WISHLIST))
+get_pages(ID_WISHLIST)
+print_pages(ID_WISHLIST)
